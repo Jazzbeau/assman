50fbfc3f2a96ba59eb6f251a0c625dd59fca7431
jazz <jazz@jazzbe.au>
2026-01-26 00:42:23 +1030

    setup dependencies

--- main.py
+++ main.py
@@ -1,6 +1,33 @@
-def main():
-    print("Hello from assman!")
+import asyncio
+import time
+
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+
+from models.broadcaster import Broadcaster
+from models.managed_app import test_app
+
+app = FastAPI()
+broadcaster = Broadcaster()
 
 
-if __name__ == "__main__":
-    main()
+@app.get("/testwindows")
+async def test_windows():
+    await test_app()
+
+
+@app.websocket("/ws")
+async def websocket_test(websocket: WebSocket):
+    await websocket.accept()
+    await broadcaster.connect(websocket)
+    print("Client connected")
+    try:
+        while True:
+            await asyncio.sleep(1)
+            await broadcaster.broadcast(
+                {"message": f"{time.strftime("%I:%M:%S%p on %B %d, %Y")}!!!!"}
+            )
+            # await websocket.receive_text()
+    except WebSocketDisconnect:
+        print("Client disconnected")
+    finally:
+        await broadcaster.disconnect(websocket)
